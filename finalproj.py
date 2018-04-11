a0ec44ba15b8364c025bfc002c97cf300429098f
Emma Shpiz <eshpiz@umich.edu>
2018-04-11 13:52:16 -0400

    movies class created and into db

--- finalproj.py
+++ finalproj.py
@@ -1,16 +1,17 @@
 #Emma Shpiz Final Project
 
-import secrets
 from bs4 import BeautifulSoup
 import requests
 import json
+import sqlite3
+import sys
+
 
 
 #Caching top movies list
-#
-CACHE_FNAME = 'top_movies.json'
+
 try:
-    cache_file = open(CACHE_FNAME, 'r')
+    cache_file = open('top_movies.json', 'r')
     cache_contents = cache_file.read()
     CACHE_DICTION = json.loads(cache_contents)
     cache_file.close()
@@ -33,11 +34,39 @@ def make_request_using_cache(url):
         resp = requests.get(url)
         CACHE_DICTION[unique_ident] = resp.text
         dumped_json_cache = json.dumps(CACHE_DICTION)
-        fw = open(CACHE_FNAME,"w")
+        fw = open('top_movies.json',"w")
         fw.write(dumped_json_cache)
         fw.close() # Close the open file
         return CACHE_DICTION[unique_ident]
 
+
+
+class TopMovies:
+  def __init__(self, rank, title, year, site_url=None):
+    self.rank = rank
+    self.title = title
+    self.year = year
+    self.url = site_url
+
+    if site_url == None:
+      self.user_rating = " "
+      self.director = " "
+      self.content_rating = " "
+      self.length = " "
+      self.genre = " "
+
+    else:
+      html3 = make_request_using_cache(site_url)
+      page_soup = BeautifulSoup(html3, 'html.parser')
+      data = page_soup.find('div',id="main_top")
+      self.user_rating = data.find('span', itemprop="ratingValue")
+      self.director = data.find('span', itemprop="director")
+      self.content_rating = data.find('meta', itemprop="contentRating")
+      self.length = data.find('time', itemprop="duration")
+      self.genre = data.find_all('span', itemprop="genre")
+
+  def __str__(self):
+    return (self.rank + self.title + self.year)
 
 def get_movie_data():
   url = 'http://www.imdb.com/chart/top?ref_=nv_mv_250_6'
@@ -53,27 +82,59 @@ def get_movie_data():
       if len(table_titles) == 5:
           titles = table_titles[1].text.strip().split(DELIMITER)
           titles_list.append(titles)
-  print(titles_list)
+  #print(titles_list)
   return titles_list
 
+movie_class = []
+for movie in get_movie_data():
+  movie_class += [TopMovies(movie[0], movie[1], movie[2])]
 
-# movies_dict = {}
-# for num in range(300):
-#   movies_dict.update(get_movie_data())
+movies_dict = {}
+for title in get_movie_data():
+  movies_dict[title[1]] = {'year':title[2], 'rank':title[0]}
+#print(movies_dict)
+#movies_dict.update(get_movie_data())
+#print
 
-x = get_movie_data()
+try:
+  movies_dict = {}
+  for title in get_movie_data():
+    movies_dict[title[1]] = {'year':title[2], 'rank':title[0]}
+except:
+  None
+for x in movie_class:
+  print(x)
 
-f = open("top_movies.json", "w")
-json_string = json.dumps(x)
-f.write(json_string)
-f.close()
+#
+# with f as open(r'top_movies', 'wb'):
+#     f.write(str(sliced_data))
+#     f.close()
+
+conn = sqlite3.connect('movies.db')
+cur = conn.cursor()
+movies_drop = 'DROP TABLE IF EXISTS "Movies"'
 
 
-# titles = search_titles.find('a')
+cur.execute(movies_drop)
 
-# for x in search_titles:
-#     print(x.get_text())
-# titles = []
-# for i in search_titles:
-#     titles.append({"title":i.get_text()})
-# print(titles)
+#Create movies
+statement = ''' CREATE TABLE 'Movies' (
+    'Id' INTEGER PRIMARY KEY,
+    'Rank' TEXT NOT NULL,
+    'Title' TEXT NOT NULL,
+    'Year' TEXT
+);
+'''
+cur.execute(statement)
+conn.commit()
+
+
+conn = sqlite3.connect('movies.db')
+cur = conn.cursor()
+for x in movie_class:
+    query = '''INSERT INTO Movies (Id, Rank, Title, Year) VALUES (?,?,?,?)
+    '''
+    data = (None, x.rank, x.title, x.year)
+    cur.execute(query, data)
+conn.commit()
+conn.close()
