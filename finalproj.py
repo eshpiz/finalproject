b1ad99081f3eebfbbaa65a6be8d0964d27d997d0
Emma Shpiz <eshpiz@umich.edu>
2018-04-17 11:58:26 -0400

    scraping data added

--- finalproj.py
+++ finalproj.py
@@ -58,10 +58,12 @@ class TopMovies:
       html3 = make_request_using_cache(movie_url)
       page_soup = BeautifulSoup(html3, 'html.parser')
       data = page_soup.find('div',id="main_top")
-      self.director = data.find('span', itemprop="director")
-      self.content_rating = data.find('meta', itemprop="contentRating")
-      self.length = data.find('time', itemprop="duration")
-      self.genre = data.find_all('span', itemprop="genre")
+      self.director = data.find('span', itemprop="director").text
+      self.content_rating = data.find('meta', itemprop="contentRating").text
+      self.length = data.find('time', itemprop="duration").text
+      #temp = data.find_all('span', itemprop="genre")
+      temp = [link.string for link in data.find_all('span', itemprop="genre")]
+      self.genre = ",".join(temp)
 
   def __str__(self):
     return (self.rank + self.title + self.year)
@@ -95,94 +97,92 @@ def get_more_movie_data():
   for x in title_col:
     movie_url = base_url + x.a['href']
     url_list.append(movie_url)
-  print(url_list)
+  #print(url_list)
   return(url_list)
 
 
-get_more_movie_data()
+x = get_more_movie_data()
 
 
 
 
 
 movie_class = []
-for movie in get_movie_data():
-  movie_class += [TopMovies(movie[0], movie[1], movie[2])]
+counter = 0
+for movie in get_movie_data()[:10]:
+  movie_class += [TopMovies(movie[0], movie[1], movie[2], movie_url = x[counter])]
+  #movie_class[counter].movie_url = x[counter]
+  counter += 1
+#print(movie_class)
+for x in movie_class:
+  print(x.length)
 # for x in get_more_movie_data():
 #   movie_class += [TopMovies(x[3])]
 
-#
-# movies_dict = {}
-# for title in get_movie_data():
-#   movies_dict[title[1]] = {'year':title[2], 'rank':title[0]}
 
 #put info in movie class
 try:
   movies_dict = {}
   for title in get_movie_data():
     movies_dict[title[1]] = {'year':title[2], 'rank':title[0]}
+    #print(movies_dict)
 except:
   None
 # for x in movie_class:
 #   print(x)
+def create_table():
+  conn = sqlite3.connect('movies.db')
+  cur = conn.cursor()
+  movies_drop = 'DROP TABLE IF EXISTS "Movies"'
+  cur.execute(movies_drop)
 
-conn = sqlite3.connect('movies.db')
-cur = conn.cursor()
-movies_drop = 'DROP TABLE IF EXISTS "Movies"'
+  #Create movies
+  statement = ''' CREATE TABLE 'Movies' (
+      'Id' INTEGER PRIMARY KEY,
+      'Rank' INTEGER NOT NULL,
+      'Title' TEXT NOT NULL,
+      'Year' Integer
+  );
+  '''
+  cur.execute(statement)
+  conn.commit()
 
+  conn = sqlite3.connect('movies.db')
+  cur = conn.cursor()
+  for x in movie_class:
+      query = '''INSERT INTO Movies (Id, Rank, Title, Year) VALUES (?,?,?,?)
+      '''
+      data = (None, x.rank, x.title, x.year)
+      cur.execute(query, data)
+  conn.commit()
+  conn.close()
 
-cur.execute(movies_drop)
+  # Deletes white space in title column
+  conn = sqlite3.connect('movies.db')
+  cur = conn.cursor()
+  statement = ''' UPDATE Movies
+      SET Title = LTRIM(Title)'''
+  cur.execute(statement)
 
-#Create movies
-statement = ''' CREATE TABLE 'Movies' (
-    'Id' INTEGER PRIMARY KEY,
-    'Rank' INTEGER NOT NULL,
-    'Title' TEXT NOT NULL,
-    'Year' Integer
-);
-'''
-cur.execute(statement)
-conn.commit()
-
-conn = sqlite3.connect('movies.db')
-cur = conn.cursor()
-for x in movie_class:
-    query = '''INSERT INTO Movies (Id, Rank, Title, Year) VALUES (?,?,?,?)
-    '''
-    data = (None, x.rank, x.title, x.year)
-    cur.execute(query, data)
-conn.commit()
-conn.close()
-
-# Deletes white space in title column
-conn = sqlite3.connect('movies.db')
-cur = conn.cursor()
-statement = ''' UPDATE Movies
-    SET Title = LTRIM(Title)'''
-cur.execute(statement)
-
-# Adding director, contentrating, length, and genre to table
-statement1 = ''' ALTER TABLE Movies
-    ADD Director TEXT'''
-cur.execute(statement1)
-
-statement2 = ''' ALTER TABLE Movies
-    ADD ContentRating TEXT'''
-cur.execute(statement2)
-
-statement3 = ''' ALTER TABLE Movies
-    ADD Length INTEGER'''
-cur.execute(statement3)
-
-statement4 = ''' ALTER TABLE Movies
-    ADD Genre TEXT'''
-cur.execute(statement4)
+  # Adding director, contentrating, length, and genre to table
+  statement1 = ''' ALTER TABLE Movies
+      ADD Director TEXT'''
+  cur.execute(statement1)
+  statement2 = ''' ALTER TABLE Movies
+      ADD ContentRating TEXT'''
+  cur.execute(statement2)
+  statement3 = ''' ALTER TABLE Movies
+      ADD Length INTEGER'''
+  cur.execute(statement3)
+  statement4 = ''' ALTER TABLE Movies
+      ADD Genre TEXT'''
+  cur.execute(statement4)
 
 
-# statement2 = ''' INSERT INTO Movies (Director)
-#   VALUES (?) '''
-# data = (x['director'])
-# cur.execute(statement2, data)
+  # statement2 = ''' INSERT INTO Movies (Director)
+  #   VALUES (?) '''
+  # data = (x['director'])
+  # cur.execute(statement2, data)
 
-conn.commit()
-conn.close()
+  conn.commit()
+  conn.close()
